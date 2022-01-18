07f05a4c49067013923e9cc3a428a15195c4957e
nikita.krivoshey <krivoshey.nikita1998@gmail.com>
2022-01-18 19:06:26 +0300

    2-pick done

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+  let i = 0;
 
+  return Object.fromEntries(
+    Object.entries(obj).filter(function ([key, elem]) {
+      if (key === fields[i++]) {
+        return true;
+      }
+    })
+  );
 };
