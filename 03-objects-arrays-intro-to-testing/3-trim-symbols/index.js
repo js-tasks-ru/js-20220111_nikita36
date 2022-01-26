016b82969c9f4d8b85c12777d485ef7eb86f3be9
nikita.krivoshey <krivoshey.nikita1998@gmail.com>
2022-01-26 19:20:52 +0300

    3-trim-symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,28 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return '';
+  }
 
+  if (size === undefined) {
+    return string;
+  }
+
+  let count = 0;
+  const finalString = [];
+
+  for (const letter of string) {
+    if (finalString[finalString.length - 1] === letter) {
+      if (count < size) {
+        finalString.push(letter);
+        count++;
+      }
+    } else {
+      count = 1;
+      finalString.push(letter);
+    }
+  }
+
+  return finalString.join('');
 }
